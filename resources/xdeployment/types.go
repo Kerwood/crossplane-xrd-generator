07e9972a2bd9637c6b3bd541926fc8903d6c829d
Patrick Kerwood <patrick@kerwood.dk>
2026-02-15 16:10:28 +0100

    updated xdeployment

--- resources/xdeployment/types.go
+++ resources/xdeployment/types.go
@@ -13,13 +13,23 @@ type XDeployment struct {
 }
 
 type XDeploymentSpec struct {
-	Image    string            `json:"image" required:"true"`
-	Replicas *int32            `json:"replicas,omitempty"`
-	Port     *int              `json:"port,omitempty"`
-	Hostname string            `json:"hostname,omitempty"`
-	Env      map[string]string `json:"env,omitempty"`
+	Image              string            `json:"image" required:"true"`
+	Tag                string            `json:"tag" required:"true"`
+	Replicas           *int32            `json:"replicas,omitempty"`
+	Port               *int              `json:"port,omitempty"`
+	Hostname           *string           `json:"hostname,omitempty"`
+	ServiceAccountName *string           `json:"serviceAccountName,omitempty"`
+	Env                map[string]string `json:"env,omitempty"`
+	SingleSignOn       *SingleSignOn     `json:"singleSignOn,omitempty"`
 }
 
-type XDeploymentStatus struct {
-	Replicas int `json:"replicas,omitempty"`
+type XDeploymentStatus struct{}
+
+type SingleSignOn struct {
+	EnableAuthProxy            bool                        `json:"enableAuthProxy,omitempty"`
+	ConnectionDetailsSecretRef *ConnectionDetailsSecretRef `json:"connectionDetailsSecretRef,omitempty"`
+}
+
+type ConnectionDetailsSecretRef struct {
+	Name string `json:"name" required:"true"`
 }
