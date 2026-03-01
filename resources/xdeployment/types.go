b5df76c3c9b442df021891b96c5b00d7ace62d7c
Patrick Kerwood <patrick@kerwood.dk>
2026-03-01 12:34:24 +0100

    updated resources to support kubebuilder

--- resources/xdeployment/types.go
+++ resources/xdeployment/types.go
@@ -4,20 +4,33 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+// +kubebuilder:object:root=true
 type XDeployment struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
-
-	Spec   XDeploymentSpec   `json:"spec"`
-	Status XDeploymentStatus `json:"status,omitempty"`
+	Spec              XDeploymentSpec   `json:"spec"`
+	Status            XDeploymentStatus `json:"status,omitempty"`
 }
 
 type XDeploymentSpec struct {
-	Image              string            `json:"image" required:"true"`
-	Tag                string            `json:"tag" required:"true"`
-	Replicas           *int32            `json:"replicas,omitempty"`
-	Port               *int              `json:"port,omitempty"`
-	Hostname           *string           `json:"hostname,omitempty"`
+	// +kubebuilder:validation:MinLength=1
+	Image string `json:"image"`
+
+	// +kubebuilder:validation:MinLength=1
+	Tag string `json:"tag"`
+
+	// +kubebuilder:default=1
+	// +kubebuilder:validation:Minimum=1
+	// +kubebuilder:validation:Maximum=50
+	Replicas *int32 `json:"replicas,omitempty"`
+
+	// +kubebuilder:validation:Minimum=1
+	// +kubebuilder:validation:Maximum=65535
+	Port *int32 `json:"port,omitempty"`
+
+	// +kubebuilder:validation:Pattern=`^[a-z0-9][a-z0-9\-\.]+[a-z0-9]$`
+	Hostname *string `json:"hostname,omitempty"`
+
 	ServiceAccountName *string           `json:"serviceAccountName,omitempty"`
 	Env                map[string]string `json:"env,omitempty"`
 	SingleSignOn       *SingleSignOn     `json:"singleSignOn,omitempty"`
@@ -26,10 +39,12 @@ type XDeploymentSpec struct {
 type XDeploymentStatus struct{}
 
 type SingleSignOn struct {
+	// +kubebuilder:default=false
 	EnableAuthProxy            bool                        `json:"enableAuthProxy,omitempty"`
 	ConnectionDetailsSecretRef *ConnectionDetailsSecretRef `json:"connectionDetailsSecretRef,omitempty"`
 }
 
 type ConnectionDetailsSecretRef struct {
-	Name string `json:"name" required:"true"`
+	// +kubebuilder:validation:MinLength=1
+	Name string `json:"name"`
 }
